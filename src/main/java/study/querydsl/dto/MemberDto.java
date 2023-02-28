cd546fdb220589ffdc569ec13fcd3d758161262d
Juwon Park <devprofessionalism@gmail.com>
2023-02-28 21:51:43 +0900

    프로젝션과 결과 반환 - @QueryProjection

--- src/main/java/study/querydsl/dto/MemberDto.java
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,7 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
+
 import lombok.Data;
 
 @Data
@@ -11,6 +13,7 @@ public class MemberDto {
 	public MemberDto() {
 	}
 
+	@QueryProjection
 	public MemberDto(String username, int age) {
 		this.username = username;
 		this.age = age;
